2308b93ca44998a0a38d3e61f9bfaf45a049675d
elizabeth <development@symbolix.com.au>
2018-11-21 08:50:10 +1100

    fixes #3

--- run_docker.sh
+++ run_docker.sh
@@ -4,18 +4,18 @@
 # use -it to start in interactive mode
 # --rm removes the container on exit
 
-#sudo docker run -d --rm \   
+#sudo docker run -d --rm \
 #    -p 28787:8787 \                         # map ports
 #    --name hello-world2 \                   # name container
 #    -e USERID=$UID \                        # you need to share a UID so you can write to mount file on host
-#    -e PASSWORD=rstudio \                   # set rstudio password - user is rstudio
-#    -v $DATA_DIR:/home/rstudio/Data \       # mount data directory to pick up changes or write to host 
+#    -e PASSWORD=SoSecret! \                   # set rstudio password - user is rstudio
+#    -v $DATA_DIR:/home/rstudio/Data \       # mount data directory to pick up changes or write to host
 #       rstudio/hello-world                  # the name of the image
 
 # simple one liner for command line copying
 
 DATA_DIR=${PWD}/Data
-sudo docker run -d --rm -p 28787:8787 --name hello-world2 -e USERID=$UID -e PASSWORD=rstudio -v $DATA_DIR:/home/rstudio/Data rstudio/hello-world
+sudo docker run -d --rm -p 28787:8787 --name hello-world2 -e USERID=$UID -e PASSWORD=SoSecret! -v $DATA_DIR:/home/rstudio/Data rstudio/hello-world
 
 # this is an example of running the container in interactive mode and logging into a bash shell
 # sudo docker run -it --rm  --name hello-world2 -e USERID=$UID -v $DATA_DIR:/home/rstudio/Data rstudio/hello-world  /bin/bash
